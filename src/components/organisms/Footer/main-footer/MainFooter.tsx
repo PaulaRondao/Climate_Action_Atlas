19084d9a61938454966ccd122bd99d8b320f0ccf
PaulaRondao <maud.rondao@gmail.com>
2025-06-06 09:41:35 +0200

    new logo changed

--- src/components/organisms/Footer/main-footer/MainFooter.tsx
+++ src/components/organisms/Footer/main-footer/MainFooter.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import { MainFooterContainer, MainInfoContainer } from './mainFooter.styles';
 import ContentInfo from '../content-info/ContentInfo';
-import SVGIMG from '../../../../../public/logo/logo-climate-white.svg';
+import SVGIMG from '../../../../../public/logo/logo-climate-green.svg';
 
 const MainFooter: React.FC = () => (
   <MainFooterContainer>
