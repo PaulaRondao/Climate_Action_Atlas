49cd6a5d789e279b98ddcc445d43a831f76beebc
PaulaRondao <maud.rondao@gmail.com>
2025-08-08 13:54:30 +1200

    update mapView, change dockerfile

--- src/components/molecules/MapView/MapView.tsx
+++ src/components/molecules/MapView/MapView.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import * as React from 'react';
+import { CSSProperties, useState } from 'react';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import 'maplibre-theme/icons.default.css';
 import 'maplibre-theme/modern.css';
@@ -10,10 +10,22 @@ import {
   RMarker,
   RNavigationControl,
   RGradientMarker,
+  RLayer,
+  RSource,
 } from 'maplibre-react-components';
 import { MapLayerMouseEvent } from 'maplibre-gl';
-import { useState } from 'react';
 import { mountainIconFactory } from './util';
+import { townData } from './util';
+import { LayerSwitcherControl, type StyleID } from './LayerSwitcherControl';
+
+const mapCSS: CSSProperties = {
+  minHeight: 500,
+};
+
+const townFillPaint = {
+  'fill-outline-color': 'rgba(0,0,0,0.1)',
+  'fill-color': 'rgba(0,0,0,0.3)',
+};
 
 const mountain: [number, number] = [6.4546, 46.1067];
 
@@ -21,17 +33,25 @@ function MapView() {
   const [markerPosition, setMarkerPosition] = useState<
     null | [number, number]
   >();
+  const [style, setStyle] = useState<StyleID>('OSM Bright');
+
+  const styles: Partial<Record<StyleID, string>> = {
+    'OSM Bright':
+      'https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json',
+  };
+
   function handleClick(e: MapLayerMouseEvent) {
     setMarkerPosition(e.lngLat.toArray());
   }
+
   return (
     <RMap
-      style={{ width: '100%', height: '500px' }}
+      mapStyle={styles[style]}
+      style={mapCSS}
       minZoom={6}
       onClick={handleClick}
       initialCenter={mountain}
       initialZoom={8}
-      mapStyle="https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
     >
       <RNavigationControl position="top-right" visualizePitch={true} />
       <RGradientMarker
@@ -48,6 +68,14 @@ function MapView() {
             color="#285daa"
             longitude={markerPosition[0]}
             latitude={markerPosition[1]}
+          />
+          <RSource key="town" id="town" type="geojson" data={townData} />
+          <RLayer
+            key="town-fill"
+            id="town-fill"
+            source="town"
+            type="fill"
+            paint={townFillPaint}
           />
         </>
       )}
