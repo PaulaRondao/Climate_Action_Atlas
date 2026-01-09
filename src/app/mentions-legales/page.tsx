bace5880fee4748908c89e193343ce71baecf1ec
PaulaRondao <maud.rondao@gmail.com>
2026-01-09 10:28:13 +0100

    add legacy notice in comment to fix error

--- src/app/mentions-legales/page.tsx
+++ src/app/mentions-legales/page.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+// import React from 'react';
 
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/next-auth/authOptions';
-import { Navigation } from '@/components/organisms';
-import { LegalNoticesPage } from '@/components/templates';
+// import { getServerSession } from 'next-auth';
+// import { authOptions } from '@/lib/next-auth/authOptions';
+// import { Navigation } from '@/components/organisms';
+// import { LegalNoticesPage } from '@/components/templates';
 
-export default async function LegalNotices() {
-  const session = await getServerSession(authOptions);
-  return (
-    <>
-      <Navigation session={session} />
-      <LegalNoticesPage />
-    </>
-  );
-}
+// export default async function LegalNotices() {
+//   const session = await getServerSession(authOptions);
+//   return (
+//     <>
+//       <Navigation session={session} />
+//       <LegalNoticesPage />
+//     </>
+//   );
+// }
