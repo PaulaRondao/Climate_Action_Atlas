18d1e6a4a3a2f55d378774821969b64f0839c94e
PaulaRondao <maud.rondao@gmail.com>
2026-01-09 16:16:35 +0100

    add and fix error with server

--- src/app/politique-de-confidentialite/page.tsx
+++ src/app/politique-de-confidentialite/page.tsx
@@ -1,16 +1,16 @@
+import { Navigation } from '@/components/organisms';
+import LegacyPolicyTemplate from '@/components/templates/LegacyPolicyTemplate/LegacyPolicyTemplate';
+import { authOptions } from '@/lib/next-auth/authOptions';
+import { getServerSession } from 'next-auth';
 import React from 'react';
 
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/next-auth/authOptions';
-import { Navigation } from '@/components/organisms';
-import { LegacyPolicyPage } from '@/components/templates';
 
 export default async function LegacyPolicy() {
   const session = await getServerSession(authOptions);
   return (
     <>
       <Navigation session={session} />
-      <LegacyPolicyPage />
+      <LegacyPolicyTemplate />
     </>
   );
 }
