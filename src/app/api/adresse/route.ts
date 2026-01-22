871a4b80bfc75b01f85919d6642d86b68d2a15e7
PaulaRondao <maud.rondao@gmail.com>
2026-01-22 21:52:12 +0100

    update API ban and update next

--- src/app/api/adresse/route.ts
+++ src/app/api/adresse/route.ts
@@ -4,9 +4,7 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get('q');
 
-  const res = await fetch(
-    'https://data.geopf.fr/geocodage/search/?q=' + query,
-  );
+  const res = await fetch('https://data.geopf.fr/geocodage/search/?q=' + query);
   const data = await res.json();
 
   return NextResponse.json(data);
