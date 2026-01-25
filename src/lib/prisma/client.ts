6858d0a4f7eb47d0545e0da9be0c48442dd21ef3
PaulaRondao <maud.rondao@gmail.com>
2026-01-25 17:08:24 +0100

    fix prettier issues

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -3,12 +3,10 @@ import { PrismaClient } from '@prisma/client';
 const prismaClientSingleton = () => new PrismaClient();
 
 declare global {
-
   var prisma: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
-export const prisma =
-  globalThis.prisma ?? prismaClientSingleton();
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma;
